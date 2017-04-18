04ebdcc8dac546d446f8501994e83824ce75ecc1
DESKTOP-O9T4I71\creat <devmob.std@gmail.com>
2017-04-18 20:53:03 +0200

    GUI Prepered for Medium DATABASE

--- src/az/mb/ViewController.java
+++ src/az/mb/ViewController.java
@@ -24,17 +24,17 @@ import javafx.stage.Stage;
 public class ViewController implements Initializable {
     
     @FXML
-    private TableView<StudentPojo> tUzytkownicy;
+    private TableView<StudentPojo> tDruzyna;
     @FXML
-    private TableView<DzialPojo> tDzialy;
+    private TableView<DzialPojo> tKara;
     @FXML
-    private TableColumn<StudentPojo,String> nick,email;
+    private TableColumn<StudentPojo,String> idMiasta_druzyna, nazwaMiasta_druzyna;
     @FXML
-    private TableColumn<StudentPojo,String> nazwa;
+    private TableColumn<StudentPojo,String> idPilkarza_kara;
     @FXML
-    private TableColumn<StudentPojo,Integer> isAdmin,id;
+    private TableColumn<StudentPojo,Integer> punkty, idDruzyna;
     @FXML
-    private TableColumn<StudentPojo,Integer> idUzytkownika, id_Dzialu;
+    private TableColumn<StudentPojo,Integer> idDruzyny_kara, idSpotkania_kara;
     @FXML
     private ObservableList<StudentPojo> list = FXCollections.observableArrayList();
     @FXML
@@ -48,7 +48,7 @@ public class ViewController implements Initializable {
 
         new DialogStudent().start(new Stage());
         list = ssql.listStudent();
-        tUzytkownicy.setItems(list);
+        tDruzyna.setItems(list);
     }
     @FXML
     private void update(ActionEvent event){
@@ -58,22 +58,22 @@ public class ViewController implements Initializable {
     private void refresh(ActionEvent event){
         list = ssql.listStudent();
         listDzialy = ssql.listDzialy();
-        tUzytkownicy.setItems(list);
-        tDzialy.setItems(listDzialy);
+        tDruzyna.setItems(list);
+        tKara.setItems(listDzialy);
     }
     @FXML
     private void delete(){
 
-        StudentPojo pojo = tUzytkownicy.getSelectionModel().getSelectedItem();
+        StudentPojo pojo = tDruzyna.getSelectionModel().getSelectedItem();
         ssql.deleteStudent(pojo.getId());
         System.out.println(pojo.getId());
         list = ssql.listStudent();
-        tUzytkownicy.setItems(list);
+        tDruzyna.setItems(list);
     }
     @FXML
     private void selectRow(MouseEvent event){
         if(event.getClickCount()==2){           
-           StudentPojo pojo = tUzytkownicy.getSelectionModel().getSelectedItem();
+           StudentPojo pojo = tDruzyna.getSelectionModel().getSelectedItem();
             StudenUpdateViewController.id = pojo.getId();
             System.out.println(StudenUpdateViewController.id);
             new StudentUppdateModel().start(new Stage());
@@ -83,21 +83,21 @@ public class ViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) { 
         
-        id.setCellValueFactory(new PropertyValueFactory<StudentPojo,Integer>("id"));
-        nick.setCellValueFactory(new PropertyValueFactory<StudentPojo,String>("nick"));
-        email.setCellValueFactory(new PropertyValueFactory<StudentPojo,String>("email"));
-        isAdmin.setCellValueFactory(new PropertyValueFactory<StudentPojo,Integer>("isAdmin"));
+        idDruzyna.setCellValueFactory(new PropertyValueFactory<StudentPojo,Integer>("id"));
+        idMiasta_druzyna.setCellValueFactory(new PropertyValueFactory<StudentPojo,String>("nick"));
+        nazwaMiasta_druzyna.setCellValueFactory(new PropertyValueFactory<StudentPojo,String>("email"));
+        punkty.setCellValueFactory(new PropertyValueFactory<StudentPojo,Integer>("isAdmin"));
 
-        id_Dzialu.setCellValueFactory(new PropertyValueFactory<StudentPojo,Integer>("id_dzialu"));
-        idUzytkownika.setCellValueFactory(new PropertyValueFactory<StudentPojo,Integer>("id_uzytkownika"));
-        nazwa.setCellValueFactory(new PropertyValueFactory<StudentPojo,String>("nazwa"));
+        idSpotkania_kara.setCellValueFactory(new PropertyValueFactory<StudentPojo,Integer>("id_dzialu"));
+        idDruzyny_kara.setCellValueFactory(new PropertyValueFactory<StudentPojo,Integer>("id_uzytkownika"));
+        idPilkarza_kara.setCellValueFactory(new PropertyValueFactory<StudentPojo,String>("nazwa"));
 
-        tUzytkownicy.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        tDzialy.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tDruzyna.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tKara.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
         list = ssql.listStudent();
         listDzialy = ssql.listDzialy();
-        tUzytkownicy.setItems(list);
-        tDzialy.setItems(listDzialy);
+        tDruzyna.setItems(list);
+        tKara.setItems(listDzialy);
     }
 }
